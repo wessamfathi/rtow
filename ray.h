a334cf5964476605aa1a9c2a1ebf03d766ef8b8c
NoXDev <wessamfathi@gmail.com>
2018-04-15 00:18:34 +0200

    Ch 3: added a ray, camera and color.

---
+++ ray.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "vec3.h"
+
+class ray
+{
+public:
+	vec3 A;
+	vec3 B;
+
+	ray() = default;
+	ray(const vec3& a, const vec3& b) { A = a; B = b; }
+	vec3 origin() const{ return A; }
+	vec3 direction() const{ return B; }
+	vec3 point_at_paramter(float t) const { return A + t * B; }
+};
--- vec3.h
+++ vec3.h
@@ -9,7 +9,7 @@ class vec3
 public:
 	float e[3];
 
-	vec3() {}
+	vec3() = default;
 	vec3(float e0, float e1, float e2) { e[0] = e0; e[1] = e1; e[2] = e2; }
 	inline float x() const { return e[0]; }
 	inline float y() const { return e[1]; }
